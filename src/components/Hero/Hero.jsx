15365b87d84c5ad9767118a957ce19644f83a2e4
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-05-09 22:38:38 -0400

    Add Footer

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <img src="src/assets/profile_img.jpg" alt="" />
-      <h1><span>I'm Hillary Gilbert, </span> Software Egineer based in U.S.</h1>
+      <h1><span>I'm Hillary Gilbert, </span> Software Egineer based in the U.S.</h1>
       <p> I'm a Results-oriented Full Stack Web Developer with a proven track record of tackling complex projects utilizing React, Express, Node.js, and MongoDB.</p>
       <div className="hero-action">
         <div className="hero-connect">Connect With Me</div>
