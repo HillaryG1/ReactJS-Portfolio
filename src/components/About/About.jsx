8054da6a30afd67f74188aca2271429576874dc7
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-05-11 21:43:16 -0400

    Make About Me Responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
         <hr />
         <div className="about-achievement">
           <h1>7+</h1>
-          <p>MERN STACK PROJECTS COMPLETED </p>
+          <p>MERN PROJECTS COMPLETED </p>
         </div>
         <hr />
         <div className="about-achievement">
